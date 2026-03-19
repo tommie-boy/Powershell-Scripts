974efed48cb2c4e4fd4bed778decc470b6b4bee5
tommie-boy <38371077+tommie-boy@users.noreply.github.com>
2026-03-19 15:43:22 +0100

    Create Get-SecureBootUpdateStatus.ps1

---
+++ Secure Boot Certs/Get-SecureBootUpdateStatus.ps1
@@ -0,0 +1,66 @@
+$result = $null
+$computerInfo = Get-ComputerInfo | select-object BiosFirmwareType, OsName
+$secureBoot = if(confirm-securebootuefi -ea SilentlyContinue) { "Enabled" } else { "Disabled" } 
+$bitlockerInfo = Get-Bitlockervolume -ea silentlycontinue
+$bitlockerActive = if($bitlockerInfo.ProtectionStatus -match 'On') { $true } else { $false } 
+$kekCertificate = [System.Text.Encoding]::ASCII.GetString((Get-SecureBootUEFI KEK).bytes)
+$dbdefaultCertificate = [System.Text.Encoding]::ASCII.GetString((Get-SecureBootUEFI dbdefault).bytes)
+$dbCertificate = [System.Text.Encoding]::ASCII.GetString((Get-SecureBootUEFI db).bytes)
+
+$regKey1 = 'HKLM:\SYSTEM\CurrentControlSet\Control\SecureBoot'
+$regKey2 = 'HKLM:\SYSTEM\CurrentControlSet\Control\SecureBoot\Servicing'
+
+if(test-path $regKey1) {
+    $secureBootUpdateAvailableRaw = get-itemproperty $regKey1 | select availableupdates
+    $secureBootUpdateAvailable = if($secureBootUpdateAvailableRaw -eq 1) { $true } else { $false } 
+} else {
+    $secureBootUpdateAvailable = 'N/a'
+}
+
+if(test-path $regKey2) {
+    $secureBootUpdateServicingRaw = get-itemproperty $regKey2 | select UEFICA2023Status, WindowsUEFICA2023Capable, UEFICA2023Error, UEFICA2023ErrorEvent  
+    $secureBootUpdateServicingWindowsUEFICA2023Capable = if($secureBootUpdateServicingRaw.WindowsUEFICA2023Capable -eq 1) { $true } else { $false } 
+    $secureBootUpdateServicingUEFICA2023Status = $secureBootUpdateServicingRaw.UEFICA2023Status
+    $secureBootUpdateServicingUEFICA2023Error =  if($secureBootUpdateServicingRaw.UEFICA2023Error -eq 1) { $true } else { $false }
+    $secureBootUpdateServicingUEFICA2023ErrorEvent = $secureBootUpdateServicingRaw.UEFICA2023ErrorEvent  
+} else {
+    $secureBootUpdateServicingWindowsUEFICA2023Capable = 'N/a'
+    $secureBootUpdateServicingUEFICA2023Status = 'N/a'
+    $secureBootUpdateServicingUEFICA2023Error = 'N/a'
+    $secureBootUpdateServicingUEFICA2023ErrorEvent = 'N/a'
+}
+
+$allEventIds = @(1801,1808)
+
+$events = @(Get-WinEvent -FilterHashtable @{LogName='System'; ID=$allEventIds} -MaxEvents 2000 -ErrorAction SilentlyContinue)
+        
+$latest_1801_Event = $events | Where-Object {$_.Id -eq 1801} | Sort-Object TimeCreated -Descending | Select-Object -First 1
+$latest_1808_Event = $events | Where-Object {$_.Id -eq 1808} | Sort-Object TimeCreated -Descending | Select-Object -First 1
+
+$kekUpdated = [System.Text.Encoding]::ASCII.GetString((Get-SecureBootUEFI KEK).bytes)
+ 
+$dbdefaultCertUpdated = [System.Text.Encoding]::ASCII.GetString((Get-SecureBootUEFI dbdefault).bytes)
+ 
+$dbCertUpdated = [System.Text.Encoding]::ASCII.GetString((Get-SecureBootUEFI db).bytes)
+
+$result += 1 | select-object @{n='computerName';e={ "$($env:computername)" }}, `
+                                @{n='biosConfiguration';e={ "$($computerInfo.BiosFirmwareType)" }}, `
+                                @{n='secureboot';e={ "$secureBoot" }}, `
+                                @{n='bitlockerActive';e={ "$bitlockerActive" }}, `
+                                @{n='SecureBootUpdateAvailable';e={ "$secureBootUpdateAvailable" }}, `
+                                @{n='WindowsUEFICA2023Capable';e={ "$secureBootUpdateServicingWindowsUEFICA2023Capable" }}, `
+                                @{n='UEFICA2023Status';e={ "$secureBootUpdateServicingUEFICA2023Status" }}, `
+                                @{n='UEFICA2023Error';e={ "$secureBootUpdateServicingUEFICA2023Error" }}, `
+                                @{n='UEFICA2023ErrorEvent';e={ "$secureBootUpdateServicingUEFICA2023ErrorEvent" }}, `
+                                @{n='Event1801Time';e={ if($latest_1801_Event) { $($latest_1801_Event.TimeCreated) } else { "N/a" } }}, `
+                                @{n='Event1801Status';e={ if($latest_1801_Event) { $latest_1801_Event.LevelDisplayName } else { "N/a" } }}, `
+                                @{n='KEK Certificate';e={ if($kekUpdated -match 'Microsoft Corporation KEK 2K CA 2023') { 'Microsoft Corporation KEK 2K CA 2023' } elseif($kekUpdated -match 'Microsoft Corporation KEK CA 2011') { 'Microsoft Corporation KEK 2K CA 2011' } else { "N/a" } }}, `
+                                @{n='DBDefault Certificate PCA 2011';e={ if($dbdefaultCertUpdated -match 'Windows UEFI CA 2023') { 'Updated to Windows UEFI CA 2023' } elseif($dbdefaultCertUpdated -match 'Microsoft Windows Production PCA 2011') { 'Microsoft Windows Production PCA 2011' } else { "N/a" } }}, `
+                                @{n='DBDefault Certificate CA 2011';e={ if($dbdefaultCertUpdated -match 'Windows UEFI CA 2023') { 'Updated to Windows UEFI CA 2023' } elseif($dbdefaultCertUpdated -match 'Microsoft Corporation UEFI CA 2011') { 'Microsoft Corporation UEFI CA 2011' } else { "N/a" } }}, `
+                                @{n='DB Certificate PCA 2011';e={ if($dbCertUpdated -match 'Windows UEFI CA 2023') { 'Updated to Windows UEFI CA 2023' } elseif($dbCertUpdated -match 'Microsoft Windows Production PCA 2011') { 'Microsoft Windows Production PCA 2011' } else { "N/a" } }}, `
+                                @{n='DB Certificate CA 2011';e={ if($dbCertUpdated -match 'Windows UEFI CA 2023') { 'Updated to Windows UEFI CA 2023' } elseif($dbCertUpdated -match 'Microsoft Corporation UEFI CA 2011') { 'Microsoft Corporation UEFI CA 2011' } else { "N/a" } }}, `
+                                @{n='Event1808Time';e={ if($latest_1808_Event) { $($latest_1808_Event.TimeCreated) } else { "N/a" } }}, `
+                                @{n='Event1808Status';e={ if($latest_1808_Event) { $latest_1808_Event.LevelDisplayName } else { "N/a" } }}
+
+
+$result
